3b591eabeeb70c8aecb5ab1ff998da66d6ec62ff
xmrdot0 <yahia_2000@Live.com>
2020-12-25 17:40:02 +0200

    Shooting fixed final yarab

--- core/src/com/test/game/screens/PlayScreen.java
+++ core/src/com/test/game/screens/PlayScreen.java
@@ -6,18 +6,19 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.test.game.WorldContactListener;
 import com.test.game.Khartoosha;
 import com.test.game.sprites.Character;
+import com.test.game.sprites.Map;
+import com.test.game.sprites.PowerUps.PowerUp;
+import com.test.game.sprites.PowerUps.SpeedBoost;
+import com.test.game.sprites.Weapon;
+
+import java.util.Random;
 
 
 public class PlayScreen implements Screen
@@ -27,13 +28,12 @@ public class PlayScreen implements Screen
 
     private TextureAtlas atlas;
     private Character character;
+    private Weapon pistol;
 
     public float delta = Gdx.graphics.getDeltaTime();
 
     // Tiled map variables
-    private TmxMapLoader mapLoader;
-    private TiledMap map;
-    private OrthogonalTiledMapRenderer mapRenderer;
+    private Map map;
 
 
     // Box2d variables
@@ -44,46 +44,15 @@ public class PlayScreen implements Screen
     private OrthographicCamera gameCam;
     private Viewport viewport;
 
-    private void initMap()
+    //Powerups array that contains 1 of each type
+    private PowerUp[] PUPs = new PowerUp[PowerUp.MAXPUPS];
+
+
+
+    public PlayScreen(Khartoosha game, int charNum, int mapNum)
     {
-        mapLoader = new TmxMapLoader();
-        map = mapLoader.load("map/testmap.tmx");
 
-        mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / Khartoosha.PPM);
-
-        box2dWorld = new World(new Vector2(0, -10), true);
-        box2dDebugRenderer = new Box2DDebugRenderer();
-        box2dDebugRenderer.setDrawBodies(false); //hides physics body
-
-        BodyDef bdef = new BodyDef();
-        PolygonShape shape = new PolygonShape();
-        FixtureDef fixtureDef = new FixtureDef();
-        Body body;
-
-
-        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class))
-        {
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set((rect.getX() + rect.getWidth() / 2) / Khartoosha.PPM, (rect.getY() + rect.getHeight() / 2) / Khartoosha.PPM);
-
-            body = box2dWorld.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() / 2 / Khartoosha.PPM, rect.getHeight() / 2 / Khartoosha.PPM);
-            fixtureDef.shape = shape;
-            body.createFixture(fixtureDef);
-        }
-
-
-    }
-
-
-    public PlayScreen(Khartoosha game)
-    {
         atlas = new TextureAtlas("Characters.pack");
-
         // Reference to our game, used to set screens
         this.game = game;
 
@@ -92,28 +61,89 @@ public class PlayScreen implements Screen
 
         // Create our Box2D world, setting no gravity in X, -1 gravity in Y, and allow bodies to sleep
         box2dWorld = new World(new Vector2(0, Khartoosha.GRAVITY), true);
+        box2dWorld = new World(new Vector2(0, -10), true);
+        box2dDebugRenderer = new Box2DDebugRenderer();
+        box2dDebugRenderer.setDrawBodies(true); //hides physics body
+
+
 
         // Allows for debug lines of our box2d world.
         box2dDebugRenderer = new Box2DDebugRenderer();
 
         // Initialize map
-        initMap();
+        map = new Map(box2dWorld);
+        map.loadMap(1);
         gameCam.position.set(viewport.getWorldWidth() / 2 / Khartoosha.PPM, viewport.getWorldHeight() / 2 / Khartoosha.PPM, 0);
 
-        character = new Character(box2dWorld, this);
+
+        character = new Character(box2dWorld, this,  charNum);
+
+        // Power Ups
+        PUPs[0] = new SpeedBoost(box2dWorld);
+        PUPs[1] = new SpeedBoost(box2dWorld);
+
+
+        WorldContactListener collisionHandler = new WorldContactListener();
+        box2dWorld.setContactListener(collisionHandler);
+
+        pistol = new Weapon(box2dWorld, this, character.getBodyPosition(), 0.05f);
+
     }
 
+    /**
+     * Handles all powerups related operations
+     *  Spawns pup if it's available
+     *  Deletes pups if were picked up
+     */
+    public void handllePups()
+    {
+        Random generator = new Random();
+        // Spawn pups if there's space
+        if (PowerUp.currentPups < PowerUp.MAXPUPS)
+        {
+            // choose random powerup type to spawn
+            int indx = generator.nextInt(PowerUp.MAXPUPS) ;
+
+            PUPs[indx].spawn();
+
+        }
+
+        for (PowerUp pup:PUPs)
+        {
+            if (pup.isSpawned())
+                pup.update();
+
+        }
+
+
+    }
 
     public void handleInput()
     {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W))
+        {
             character.jump();
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.A))
+        {
             character.moveLeft();
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.D))
+        {
             character.moveRight();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.S))
+        {
+            character.moveDown();
+        }
+
+        //Debug
+        if (Gdx.input.isKeyPressed(Input.Keys.X)) {
+            System.out.println("PUP Location: "+PUPs[0].pupBody.getPosition().x + "  " + PUPs[0].pupBody.getPosition().y);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.Z)) {
+            System.out.println("Character Location: "+character.physicsBody.getPosition().x + "  " + character.physicsBody.getPosition().y);
         }
     }
 
@@ -123,13 +153,18 @@ public class PlayScreen implements Screen
         box2dWorld.step(1/60F, 6, 2);
         handleInput();
         character.update(delta);
+        pistol.update(delta);
         // Update camera position wrt character position
         gameCam.update();
         gameCam.position.x = character.physicsBody.getPosition().x;
         gameCam.position.y = character.physicsBody.getPosition().y;
 
+        //Power Ups
+        //TODO: comment handlePups to disable pups functionality
+        //handllePups();
         // Render map
-        mapRenderer.setView(gameCam);
+
+        map.mapRenderer.setView(gameCam);
     }
 
 
@@ -153,22 +188,43 @@ public class PlayScreen implements Screen
     public void render(float delta)
     {
         update();
+
+
+        if(character.isFlipX())
+        {
+            pistol.setFlip(true,false);
+            pistol.faceRight=false;
+        }
+        else if (!character.isFlipX())
+        {
+            pistol.setFlip(false,false);
+            pistol.faceRight=true;
+        }
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        mapRenderer.render();
+        map.render();
 
         box2dDebugRenderer.render(box2dWorld, gameCam.combined);
 
         game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
         character.draw(game.batch);
+
+        //TODO: uncomment when textures are ready
+        /*for (PowerUp pup:PUPs)
+        {
+            if (pup.getSpawnState())
+                pup.draw(game.batch);
+        }*/
+        pistol.draw(game.batch);
+        pistol.render(game.batch);
         game.batch.end();
     }
 
     @Override
     public void resize(int width, int height)
     {
-        System.out.println("Resized");
         viewport.update(width, height);
     }
 
